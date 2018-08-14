265cbddc4067a95d588502a73fe2a0f9c4647458
Donnie Kim <stardonnie@gmail.com>
2018-08-13 21:01:24 -0400

    [Update] Return hash code asynchronously of contract deployment

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-    HTTP_PROVIDER: process.env.HTTP_PROVIDER || 'http://localhost:8545',
-    OWNERADDRESS: process.env.OWNERADDRESS || '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
-    OWNERKEY: process.env.OWNERKEY || 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3',
-    CHAIN_ID: process.env.CHAIN_ID || 1337
+    HTTP_PROVIDER: process.env.HTTP_PROVIDER || 'https://ropsten.infura.io/nAp458BQNRGwQ3Fynkd5',
+    OWNERADDRESS: process.env.OWNERADDRESS || '0x773679a768aca758c2e4e65afa0bb94d1037ed82',
+    OWNERKEY: process.env.OWNERKEY || '1C895CF11B3D4DA06B9EBC9FC056F50FAEDB69045296009F2B19C16CDCD8C779',
+    CHAIN_ID: process.env.CHAIN_ID || 3
 }
 
 module.exports = CONFIG
--- index.js
+++ index.js
@@ -15,31 +15,36 @@ app.use(bodyParser.urlencoded({
 const config = require('./config')
 const web3 = new Web3(config.HTTP_PROVIDER)
 
-sendTransaction = async (params) => {
+let NONCE = 0;
+
+createTransaction = async (params) => {
+    if (!NONCE) {
+        NONCE = await web3.eth.getTransactionCount(params.addressFrom)
+    }
+    else {
+        NONCE += 1
+    }
+
     let chainId = parseInt(config.CHAIN_ID)
-    let nonce = await web3.eth.getTransactionCount(params.addressFrom)
     let gasPrice = await web3.eth.getGasPrice()
 
     let eth = params.eth || '0'
 
     let txParams = {
-        nonce: web3.utils.toHex(nonce),
-        gasPrice: web3.utils.toHex(gasPrice),
+        nonce: web3.utils.toHex(NONCE),
+        gasPrice: web3.utils.toHex(20000000000),
         gasLimit: web3.utils.toHex(4700000),
-        from: params.addressFrom,
-        to: params.addressTo,
         value: web3.utils.toHex(web3.utils.toWei(eth, 'ether')),
         chainId: chainId,
         data: (params.data || '')
     };
 
-    console.log(txParams)
     let tx = new Tx(txParams)
     let privateKey = new Buffer.from(params.key, 'hex')
     tx.sign(privateKey)
     let serializedTx = tx.serialize()
 
-    return web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+    return '0x' + serializedTx.toString('hex');
 }
 
 app.get('/', (req, res) => {
@@ -73,19 +78,26 @@ app.post('/deploy', async (req, res) => {
 
     // res.send('Deployed at ' + erc20.options.address + ' in ' + config.HTTP_PROVIDER)
 
+    const totalSupply = new BigNumber(req.body.totalsupply * (10 ** req.body.decimals))
     let deployCode = await contract.deploy({
         data: '0x' + bytecode,
-        arguments: ['ERC20', 'Example ERC20 Token', 18, 1000000]
+        arguments: [req.body.tick, req.body.name, req.body.decimals, totalSupply]
     }).encodeABI()
 
     let params = {
         addressFrom: config.OWNERADDRESS,
         key: config.OWNERKEY,
-        addressTo: '0x0',
         data: deployCode
     }
-    let txHash = await sendTransaction(params)
-    res.json(txHash)
+    const signedData = await createTransaction(params)
+    web3.eth.sendSignedTransaction(signedData)
+    .on('transactionHash', hash => {
+        res.json({txHash: hash, error: null})
+    })
+    .on('error', err => {
+        console.log('Error in transaction: ', err)
+        res.status(500).send({txHash: null, error: err})
+    })
 })
 
 app.listen(3000, () => console.log('Token Launcher App - listening on port 3000'))
